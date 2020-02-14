d2fb779835e2b4c2d2d94e3f6c2b627e5048886a
jahman07104 <jahman@bellatlantic.net>
2020-02-14 15:11:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(array, birthday){
+  const newArray = []
+  for(let i = 0 ; i < array.length; i++){
+  newArray.push(`Thank you, ${array[i]}, for the wonderful ${birthday} gift!`);
+  }
+  return newArray;
+}
+
+function countDown(number) {
+  while (number >= 0){
+  console.log(number--)
+  }
+} 
+
+
